9d0c3b936bfd429b09c2ddeed5ca25a74c14447e
hemanta212 <sharmahemanta.212@gmail.com>
2022-11-12 14:38:15 +0545

    Listen to a port

--- app/server.go
+++ app/server.go
@@ -2,25 +2,23 @@ package main
 
 import (
 	"fmt"
-	// Uncomment this block to pass the first stage
-	// "net"
-	// "os"
+	"net"
+	"os"
 )
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
-	// Uncomment this block to pass the first stage
-	//
-	// l, err := net.Listen("tcp", "0.0.0.0:6379")
-	// if err != nil {
-	// 	fmt.Println("Failed to bind to port 6379")
-	// 	os.Exit(1)
-	// }
-	// _, err = l.Accept()
-	// if err != nil {
-	// 	fmt.Println("Error accepting connection: ", err.Error())
-	// 	os.Exit(1)
-	// }
+	l, err := net.Listen("tcp", "0.0.0.0:6379")
+	if err != nil {
+		fmt.Printf("Error binding tcp listener: %v", err)
+		os.Exit(1)
+	}
+	fmt.Println(":: Listening on port 6379.. ")
+	_, err = l.Accept()
+	if err != nil {
+		fmt.Printf("Error accepting conns: %v", err)
+		os.Exit(1)
+	}
 }
