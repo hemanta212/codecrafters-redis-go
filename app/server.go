7fd9e0ea639d655dbba576aeaec4548500159225
hemanta212 <sharmahemanta.212@gmail.com>
2022-11-13 17:46:11 +0545

    Concurrent requests

--- app/server.go
+++ app/server.go
@@ -18,30 +18,31 @@ func main() {
 	}
 	defer l.Close()
 	fmt.Println(":: Listening on port 6379.. ")
-	// Waiting for the connection
-	conn, err := l.Accept()
-	defer conn.Close()
-	fmt.Println("Got connected", conn.RemoteAddr())
-	if err != nil {
-		fmt.Printf("Error accepting conns: %v", err)
-		os.Exit(1)
-	}
 
 	for {
-		// go func(conn net.Conn) {
-		msg := make([]byte, 4028)
-		if _, err := conn.Read(msg); err != nil {
-			if err == io.EOF {
-				break
-			} else {
-				fmt.Println("Error reading from client: ", err.Error())
-				os.Exit(1)
-			}
-
+		// Waiting for the connection
+		conn, err := l.Accept()
+		fmt.Println("Got connected", conn.RemoteAddr())
+		if err != nil {
+			fmt.Printf("Error accepting conns: %v", err)
+			os.Exit(1)
 		}
-		fmt.Println("Got command: ", string(msg))
-		conn.Write([]byte("+PONG\r\n"))
-		fmt.Println("closing connection: ", conn.RemoteAddr())
-		// }(conn)
+
+		go func(conn net.Conn) {
+			defer conn.Close()
+			msg := make([]byte, 4028)
+			if _, err := conn.Read(msg); err != nil {
+				if err == io.EOF {
+					return
+				} else {
+					fmt.Println("Error reading from client: ", err.Error())
+					os.Exit(1)
+				}
+
+			}
+			fmt.Println("Got command: ", string(msg))
+			conn.Write([]byte("+PONG\r\n"))
+			fmt.Println("closing connection: ", conn.RemoteAddr())
+		}(conn)
 	}
 }
