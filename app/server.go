d410df2f7465c914f0935e8f30f55b1d95b21ccf
hemanta212 <sharmahemanta.212@gmail.com>
2022-11-14 20:58:37 +0545

    Refactor conn handler and response

--- app/server.go
+++ app/server.go
@@ -39,53 +39,29 @@ func main() {
 func handleConn(conn net.Conn, storage *KeyValueStore) {
 	defer conn.Close()
 	for {
-		msg := make([]byte, 1024)
-		msglen, err := conn.Read(msg)
+		value, err := decodeRESP(conn)
 		if err != nil {
 			if err == io.EOF {
 				return
 			} else {
 				fmt.Println(":: Error reading from client: ", err.Error())
-				os.Exit(1)
+				return
 			}
-
 		}
-
-		message := strings.TrimSpace(string(msg[:msglen]))
-		value := decodeRESP(message)
-		command, args := strings.ToLower(value[0]), value[1:]
-
-		if command == "echo" {
-			echoedWord := args[0]
-			output := fmt.Sprintf("$%d\r\n%s\r\n", len(echoedWord), echoedWord)
-			// fmt.Println(":: Writing result as: ", strconv.Quote(output))
-			conn.Write([]byte(output))
-		} else if command == "set" {
-			key, value := args[0], args[1]
-			if len(args) > 3 {
-				px := args[3]
-				duration, _ := strconv.Atoi(px)
-				go time.AfterFunc(time.Duration(duration)*time.Millisecond, func() { storage.Delete(key) })
-			}
-			storage.Set(key, value)
-			conn.Write([]byte("+OK\r\n"))
-		} else if command == "get" {
-			value, found := storage.Get(args[0])
-			if found {
-				conn.Write([]byte(fmt.Sprintf("$%d\r\n%s\r\n", len(value), value)))
-			} else {
-				conn.Write([]byte("$-1\r\n"))
-			}
-		} else {
-			conn.Write([]byte("+PONG\r\n"))
-		}
+		responseHandler(value, conn, storage)
 	}
 }
 
-func decodeRESP(message string) []string {
+func decodeRESP(conn io.Reader) ([]string, error) {
+	msg := make([]byte, 1024)
+	value := []string{}
+	msglen, err := conn.Read(msg)
+	if err != nil {
+		return value, err
+	}
+	message := strings.TrimSpace(string(msg[:msglen]))
 	messageSlice := strings.Split(message, "\r\n")
 	fmt.Println(":: Message content: ", messageSlice)
-	value := []string{}
 	// discard the first two RESP spec keywords and pick out the commands at even interval
 	for i, item := range messageSlice[2:] {
 		if i%2 == 0 {
@@ -93,5 +69,44 @@ func decodeRESP(message string) []string {
 		}
 	}
 	fmt.Println(":: Parsed: ", value)
-	return value
+	return value, nil
+}
+
+func responseHandler(instructions []string, conn io.Writer, storage *KeyValueStore) {
+	command, args := strings.ToLower(instructions[0]), instructions[1:]
+	switch command {
+	case "echo":
+		echoedWord := args[0]
+		output := fmt.Sprintf("$%d\r\n%s\r\n", len(echoedWord), echoedWord)
+		// fmt.Println(":: Writing result as: ", strconv.Quote(output))
+		conn.Write([]byte(output))
+	case "ping":
+		conn.Write([]byte("+PONG\r\n"))
+	case "set":
+		setHandler(args, conn, storage)
+	case "get":
+		getHandler(args, conn, storage)
+	default:
+		conn.Write([]byte("+PONG\r\n"))
+	}
+}
+
+func setHandler(args []string, conn io.Writer, storage *KeyValueStore) {
+	key, value := args[0], args[1]
+	if len(args) > 3 {
+		px := args[3]
+		duration, _ := strconv.Atoi(px)
+		go time.AfterFunc(time.Duration(duration)*time.Millisecond, func() { storage.Delete(key) })
+	}
+	storage.Set(key, value)
+	conn.Write([]byte("+OK\r\n"))
+}
+
+func getHandler(args []string, conn io.Writer, storage *KeyValueStore) {
+	value, found := storage.Get(args[0])
+	if found {
+		conn.Write([]byte(fmt.Sprintf("$%d\r\n%s\r\n", len(value), value)))
+	} else {
+		conn.Write([]byte("$-1\r\n"))
+	}
 }
